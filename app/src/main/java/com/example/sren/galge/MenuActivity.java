893feb46743c35f36ee36d14c5b4d3b6268b9657
Cilian <32563603+Cilian@users.noreply.github.com>
2018-12-27 17:33:13 +0100

    Loading screen

--- app/src/main/java/com/example/sren/galge/MenuActivity.java
+++ app/src/main/java/com/example/sren/galge/MenuActivity.java
@@ -3,12 +3,14 @@ package com.example.sren.galge;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import static android.view.Window.FEATURE_NO_TITLE;
@@ -16,10 +18,14 @@ import static android.view.Window.FEATURE_NO_TITLE;
 public class MenuActivity extends Activity implements View.OnClickListener{
 
     Button option, start, highscore;
-    TextView galge1,galge2,leg1,leg2;
+    TextView galge1,galge2,leg1,leg2, preparing, wait;
+    ProgressBar progressBar;
 
-    final ScaleAnimation growAnim = new ScaleAnimation(1.0f, 1.08f, 1.0f, 1.08f, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
-    final ScaleAnimation shrinkAnim = new ScaleAnimation(1.08f, 1.0f, 1.08f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
+    TextView txt;
+    Integer count =1;
+
+     ScaleAnimation growAnim = new ScaleAnimation(1.0f, 1.08f, 1.0f, 1.08f, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
+     ScaleAnimation shrinkAnim = new ScaleAnimation(1.08f, 1.0f, 1.08f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
 
 
 
@@ -34,7 +40,10 @@ protected void onCreate(Bundle savedInstanceState){
         final FragmentManager fm = getFragmentManager();
         final PropFragment n = new PropFragment();
 
+        progressBar = findViewById(R.id.progressBar);
 
+        preparing = findViewById(R.id.preparing);
+        wait = findViewById(R.id.wait);
 
         start = findViewById(R.id.start);
         start.setOnClickListener(this);
@@ -50,13 +59,10 @@ protected void onCreate(Bundle savedInstanceState){
         highscore = findViewById(R.id.highscore);
         highscore.setOnClickListener(this);
 
-
         galge1 = findViewById(R.id.galge1);
         galge2 = findViewById(R.id.galge2);
         leg1 = findViewById(R.id.leg1);
         leg2 = findViewById(R.id.leg2);
-
-
 
 
     growAnim.setDuration(1500);
@@ -111,21 +117,55 @@ protected void onCreate(Bundle savedInstanceState){
 @Override
 public void onClick(View v){
         if(v==start){
-            openGame();
+            openLoading();
         }
         if(v == highscore){
             openScores();
         }
 }
-        public void openGame(){
-            Intent intent = new Intent(this,GameActivity.class);
-            startActivity(intent);
-        }
+
         public void openScores(){
-            Intent intent = new Intent(this,loading.class);
+            Intent intent = new Intent(this,Highscore.class);
             startActivity(intent);
         }
 
+        public void openLoading(){
+            option.setVisibility(View.INVISIBLE);
+            highscore.setVisibility(View.INVISIBLE);
+            start.setVisibility(View.INVISIBLE);
+
+            wait.setVisibility(View.VISIBLE);
+            preparing.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
+            new MyTask().execute(70);
+
+        }
+
+    class MyTask extends AsyncTask<Integer, Integer, String> {
+        @Override
+        protected String doInBackground(Integer... params) {
+            for (; count <= params[0]; count++) {
+                try {
+                    Thread.sleep(20);
+                    publishProgress(count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return "Task Completed.";
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            progressBar.setVisibility(View.GONE);
+        }
+        @Override
+        protected void onPreExecute() {
+        }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressBar.setProgress(values[0]);
+        }
+    }
 
     @Override
     public void onBackPressed() {
